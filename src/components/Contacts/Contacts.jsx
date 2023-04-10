ebc9795731ec1691a74191d9018604d4b441a1c3
Taras <tarasbohun07@gmail.com>
2023-04-10 12:35:44 +0300

    some fix

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -2,13 +2,13 @@ import { List, Item, Container } from './Contacts.styled';
 
 import { useContacts } from 'hooks/useContacts';
 import { deleteContact } from 'redux/getContacts';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectIsLoading } from 'redux/selectors';
+import { useDispatch } from 'react-redux';
+// import { selectIsLoading } from 'redux/selectors';
 
 export const Contacts = () => {
   const contacts = useContacts();
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
+  // const isLoading = useSelector(selectIsLoading);
   // const error = useSelector(selectError);
 
   return (
@@ -20,7 +20,8 @@ export const Contacts = () => {
               {name}: {phone}
             </Item>
             <button type="button" onClick={() => dispatch(deleteContact(id))}>
-              {isLoading ? 'Loading' : 'Delete'}
+              {/* {isLoading ? 'Loading' : 'Delete'} */}
+              Delete
             </button>
           </Container>
         );
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 
-// import { PersistGate } from 'redux-persist/integration/react';
 import { store } from './redux/store';
 
 import { App } from 'components/App';
